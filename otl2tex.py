bb65e54ce17de3fbfac5d7306b80b3a8601f10c2
Thomas485 <1681511+Thomas485@users.noreply.github.com>
2021-04-30 12:02:54 +0200

    apply autoformatting

--- otl2tex.py
+++ otl2tex.py
@@ -13,7 +13,7 @@ actions = {
         nominative= \"{nom}\",
         genitive= \"{gen}\",
       }}
-    }}"""
+    }}""",
 }
 
 preamble = R"""
@@ -63,54 +63,58 @@ preamble = R"""
 
 postamble = "\n\\end{document}\n"
 
+
 def map_name_section(line):
-    lst = line.split(' ')
-    if len(lst)==3:
+    lst = line.split(" ")
+    if len(lst) == 3:
         return lst
     else:
         return None
 
-def extract_name(data,s):
+
+def extract_name(data, s):
     lst = map_name_section(data)
     if lst is None:
         print("Error: Wrong syntax for NAME:", data)
         exit(-1)
     else:
-        return s.format(name=lst[0],nom=lst[1],gen=lst[2])
+        return s.format(name=lst[0], nom=lst[1], gen=lst[2])
+
 
 def process(line):
     line = line.strip()
-    if line.startswith(':'):
+    if line.startswith(":"):
         line = line[1:]
     else:
-        for name,s in actions.items():
+        for name, s in actions.items():
             idx = line.find(name)
-            if idx>=0:
-                data = line[idx+len(name)+1:]
-                if name=="NAME":
-                    line = extract_name(data,s)
+            if idx >= 0:
+                data = line[idx + len(name) + 1 :]
+                if name == "NAME":
+                    line = extract_name(data, s)
                 else:
                     line = s.format(data)
                 break
     return line
 
+
 def relevant_line(line):
     line = line.strip()
-    return (not line
-        or line.startswith(':')
-        or any([name in line for name in actions]))
+    return not line or line.startswith(":") or any([name in line for name in actions])
 
-def file_or_default(filename,default):
+
+def file_or_default(filename, default):
     if filename is None:
         return default
     else:
-        with open(filename,'r') as f:
+        with open(filename, "r") as f:
             return f.readlines()
 
-def try_load_config(args,path):
+
+def try_load_config(args, path):
     try:
         tmp = toml.load(path)
-        apply_config(args,tmp)
+        apply_config(args, tmp)
     except TomlDecodeError as e:
         print(f"Can't decode toml file '{path}': {e}")
     else:
@@ -118,53 +122,63 @@ def try_load_config(args,path):
             print("local config loaded")
 
 
-def apply_config(args,config):
-    for k,v in config.items():
+def apply_config(args, config):
+    for k, v in config.items():
         if k == "title" and args.title is None:
-            args.title=v
+            args.title = v
         elif k == "author" and args.author is None:
-            args.author=v
+            args.author = v
         elif k == "infile" and args.infile is None:
-            args.infile=v
+            args.infile = v
         elif k == "outfile" and args.outfile is None:
-            args.outfile=v
+            args.outfile = v
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-q", "--quiet", help="no status output on command line", action="store_true")
+    parser.add_argument(
+        "-q", "--quiet", help="no status output on command line", action="store_true"
+    )
     parser.add_argument("-a", "--author", help="author of document", default="me")
-    parser.add_argument("-t", "--title", help="title of the document",default="default title")
+    parser.add_argument(
+        "-t", "--title", help="title of the document", default="default title"
+    )
     parser.add_argument("--preamble", help="filename of the preamble")
     parser.add_argument("--postamble", help="filename of the postamble")
-    parser.add_argument("--config", help="defaults to .otl2tex.toml in the local directory")
-    parser.add_argument("--write-config", help="store the command line parameters to .otl2tex.toml", action="store_true")
+    parser.add_argument(
+        "--config", help="defaults to .otl2tex.toml in the local directory"
+    )
+    parser.add_argument(
+        "--write-config",
+        help="store the command line parameters to .otl2tex.toml",
+        action="store_true",
+    )
     parser.add_argument("infile", nargs="?")
     parser.add_argument("outfile", nargs="?")
     args = parser.parse_args()
 
-    try_load_config(args, args.config or os.path.join(os.getcwd(),".otl2tex.toml"))
+    try_load_config(args, args.config or os.path.join(os.getcwd(), ".otl2tex.toml"))
 
-    if args.infile is None: # TODO: better way to warn if there is no input?
+    if args.infile is None:  # TODO: better way to warn if there is no input?
         print("No input file given")
         exit(-1)
 
     # prepare arguments
     if args.outfile is None:
-        args.outfile = args.infile[:-3]+"tex"
+        args.outfile = args.infile[:-3] + "tex"
 
-    if args.outfile==args.infile:
+    if args.outfile == args.infile:
         print("Error: input-file is output-file, maybe thats not what you intended")
         exit(-1)
 
     if not args.quiet:
         print(args.infile + " -> " + args.outfile)
-    
-    args.preamble = file_or_default(args.preamble,preamble)
-    args.postamble = file_or_default(args.postamble,postamble)
 
-    with open(args.outfile,'w') as output_file, open(args.infile,'r') as input_file:
-        output_file.write(args.preamble.format(author=args.author,title=args.title))
+    args.preamble = file_or_default(args.preamble, preamble)
+    args.postamble = file_or_default(args.postamble, postamble)
+
+    with open(args.outfile, "w") as output_file, open(args.infile, "r") as input_file:
+        output_file.write(args.preamble.format(author=args.author, title=args.title))
         for line in input_file:
             if relevant_line(line):
                 output_file.write(process(line))
@@ -172,15 +186,16 @@ def main():
         output_file.write(args.postamble)
 
     if args.write_config:
-        file = os.path.join(os.path.dirname(args.infile),".otl2tex.toml")
-        with open(file,"w") as f:
+        file = os.path.join(os.path.dirname(args.infile), ".otl2tex.toml")
+        with open(file, "w") as f:
             conf = {
-                "author":args.author,
-                "title":args.title,
-                "infile":args.infile,
-                "outfile":args.outfile
+                "author": args.author,
+                "title": args.title,
+                "infile": args.infile,
+                "outfile": args.outfile,
             }
-            toml.dump(conf,f)
+            toml.dump(conf, f)
+
 
 if __name__ == "__main__":
     try:

34d7968bea677ac3da953695b27c46e44f1fa114
Thomas485 <1681511+Thomas485@users.noreply.github.com>
2023-12-03 23:04:32 +0100

    renew

--- otl2tex.py
+++ otl2tex.py
@@ -1,14 +1,13 @@
-import sys
-import argparse
+from typing_extensions import Annotated
+import typer
 import os
-import toml
 
 actions = {
     "PART": R"\part{{{}}}",
     "CHAPTER": R"\chapter{{{}}}",
     "SUBSECTION": R"\subsection{{{}}}",
     "SECTION": R"\section{{{}}}",
-    "NAME": """\directlua{{
+    "NAME": r"""\directlua{{
       names[\"{name}\"] = {{
         nominative= \"{nom}\",
         genitive= \"{gen}\",
@@ -89,7 +88,7 @@ def process(line):
         for name, s in actions.items():
             idx = line.find(name)
             if idx >= 0:
-                data = line[idx + len(name) + 1 :]
+                data = line[idx + len(name) + 1:]
                 if name == "NAME":
                     line = extract_name(data, s)
                 else:
@@ -103,103 +102,37 @@ def relevant_line(line):
     return not line or line.startswith(":") or any([name in line for name in actions])
 
 
-def file_or_default(filename, default):
-    if filename is None:
-        return default
-    else:
-        with open(filename, "r") as f:
-            return f.readlines()
+def main(infile: str,
+         outfile: str,
+         author: Annotated[str, typer.Option("--author", "-a")] = "Me",
+         title: Annotated[str, typer.Option("--title", "-t")] = "My Title",
+         quiet: Annotated[bool, typer.Option("--quiet", "-q")] = False,
+         pdf: Annotated[bool, typer.Option("--pdf")] = False,
+         ):
 
-
-def try_load_config(args, path):
-    try:
-        tmp = toml.load(path)
-        apply_config(args, tmp)
-    except TomlDecodeError as e:
-        print(f"Can't decode toml file '{path}': {e}")
-    else:
-        if not args.quiet:
-            print("local config loaded")
-
-
-def apply_config(args, config):
-    for k, v in config.items():
-        if k == "title" and args.title is None:
-            args.title = v
-        elif k == "author" and args.author is None:
-            args.author = v
-        elif k == "infile" and args.infile is None:
-            args.infile = v
-        elif k == "outfile" and args.outfile is None:
-            args.outfile = v
-
-
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "-q", "--quiet", help="no status output on command line", action="store_true"
-    )
-    parser.add_argument("-a", "--author", help="author of document", default="me")
-    parser.add_argument(
-        "-t", "--title", help="title of the document", default="default title"
-    )
-    parser.add_argument("--preamble", help="filename of the preamble")
-    parser.add_argument("--postamble", help="filename of the postamble")
-    parser.add_argument(
-        "--config", help="defaults to .otl2tex.toml in the local directory"
-    )
-    parser.add_argument(
-        "--write-config",
-        help="store the command line parameters to .otl2tex.toml",
-        action="store_true",
-    )
-    parser.add_argument("infile", nargs="?")
-    parser.add_argument("outfile", nargs="?")
-    args = parser.parse_args()
-
-    try_load_config(args, args.config or os.path.join(os.getcwd(), ".otl2tex.toml"))
-
-    if args.infile is None:  # TODO: better way to warn if there is no input?
-        print("No input file given")
-        exit(-1)
-
-    # prepare arguments
-    if args.outfile is None:
-        args.outfile = args.infile[:-3] + "tex"
-
-    if args.outfile == args.infile:
+    if outfile == infile:
         print("Error: input-file is output-file, maybe thats not what you intended")
         exit(-1)
 
-    if not args.quiet:
-        print(args.infile + " -> " + args.outfile)
+    if not quiet:
+        print(infile + " -> " + outfile)
 
-    args.preamble = file_or_default(args.preamble, preamble)
-    args.postamble = file_or_default(args.postamble, postamble)
-
-    with open(args.outfile, "w") as output_file, open(args.infile, "r") as input_file:
-        output_file.write(args.preamble.format(author=args.author, title=args.title))
+    with open(outfile, "w") as output_file, open(infile, "r") as input_file:
+        output_file.write(preamble.format(
+            author=author, title=title))
         for line in input_file:
             if relevant_line(line):
                 output_file.write(process(line))
                 output_file.write("\n")
-        output_file.write(args.postamble)
+        output_file.write(postamble)
 
-    if args.write_config:
-        file = os.path.join(os.path.dirname(args.infile), ".otl2tex.toml")
-        with open(file, "w") as f:
-            conf = {
-                "author": args.author,
-                "title": args.title,
-                "infile": args.infile,
-                "outfile": args.outfile,
-            }
-            toml.dump(conf, f)
+    if pdf:
+        os.system(f"latexmk  -pdf -pdflatex=lualatex {outfile}")
 
 
 if __name__ == "__main__":
     try:
-        main()
+        typer.run(main)
     except OSError as e:
         print("Error: ", e)
         exit(-1)
